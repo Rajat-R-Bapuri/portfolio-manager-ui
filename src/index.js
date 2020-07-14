172b185355c100b245688012551b9576a5f73e9a
Rajat-R-Bapuri <rajat.bapuri@nyu.edu>
2020-07-14 16:16:53 -0400

    add routing

--- src/index.js
+++ src/index.js
@@ -2,18 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PlotData from "./containers/chart/PlotData";
 import Profile from "./components/companyProfile/Profile";
+import PortfolioHome from "./components/portfolio/portfolioHome";
+import Login from "./components/login/login";
+import NavBar from "./components/NavBar/NavBar";
 import News from "./containers/news/News";
 import Container from "@material-ui/core/Container";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const symbol = "TSLA";
+const symbol = "Stocks";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Container maxWidth="md">
-      <PlotData symbol={symbol} />
+    <Router>
+      <Container maxWidth="md">
+        <NavBar />
+        <Switch>
+          <Route path="/news" render={() => <News query={symbol} />} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        {/* <NavBar /> */}
+        {/* <Login /> */}
+        {/* <PortfolioHome /> */}
+        {/* <PlotData symbol={symbol} />
       <Profile symbol={symbol} />
-      <News query={symbol} />
-    </Container>
+      <News query={symbol} /> */}
+      </Container>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
