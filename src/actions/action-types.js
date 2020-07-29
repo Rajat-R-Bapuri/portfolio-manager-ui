130f7bb18faf6c0a05a5296bfc8dd90de45df948
Rajat-R-Bapuri <rajat.bapuri@nyu.edu>
2020-07-29 15:19:52 -0400

    add stock prices received

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -2,8 +2,11 @@ export const USER_LOGGED_IN = "userLoggedIn";
 export const USER_LOGGED_OUT = "userLoggedOut";
 export const USER_PROFILE_LOADED = "userProfileLoaded";
 export const USER_PROFILE_LOAD_FAILED = "userProfileLoadFailed";
-export const STOCK_HISTORY_LOADED = "stockHistoryLoaded";
+
 export const NEWS_LOADED = "newsLoaded";
-export const STOCK_NEWS_LOADED = "stockNewsLoaded";
 export const COMPANY_PROFILE_LOADED = "companyProfileLoaded";
+
+export const STOCK_NEWS_LOADED = "stockNewsLoaded";
+export const STOCK_HISTORY_LOADED = "stockHistoryLoaded";
 export const STOCKS_SYMBOLS_LOADED = "stocksSymbolsLoaded";
+export const STOCKS_PRICES_RECEIVED = "stocksPricesReceived";
--- src/actions/stocks-actions.js
+++ src/actions/stocks-actions.js
@@ -1,8 +1,15 @@
-import { STOCKS_SYMBOLS_LOADED } from "./action-types";
+import { STOCKS_SYMBOLS_LOADED, STOCKS_PRICES_RECEIVED } from "./action-types";
 
 export function stocksSymbolsLoaded(payload) {
   return {
     type: STOCKS_SYMBOLS_LOADED,
+    payload,
+  };
+}
+
+export function stocksPricesReceived(payload) {
+  return {
+    type: STOCKS_PRICES_RECEIVED,
     payload,
   };
 }
